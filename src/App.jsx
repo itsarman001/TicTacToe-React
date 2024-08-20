5b03ee6139b58a9dc6bb47dd1194d327929d5d6a
Arman <armanar6712@gmail.com>
2024-08-20 23:42:45 +0530

    Implement tic-tac-toe game logic and handle player moves

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
+import TicTocToe from "./conponents/TicTacToe";
+import TicTacToe from "./conponents/TicTacToe";
 
 function App() {
-  let data = ["", "", "", "", "", "", "", "", ""];
-  const [count, setCount] = useState(0);
-  const [lock, setLock] = useState(false);
-
-  const toggle = (e, num) => {
-    if (lock) return 0;
-    
-    count % 2 === 0 ? (e.target.innerHTML = "X") : (e.target.innerHTML = "O");
-    data[num] = count % 2 === 0 ? "X" : "O";
-    setCount((prevCount) => prevCount + 1);
-  };
-
   return (
     <div className="w-screen bg-slate-700 h-screen flex items-center justify-start flex-col">
       <h2 className="text-3xl text-center text-white py-3 font-semibold uppercase">
         Tic Tac Toe <span className="text-cyan-300">React</span>
       </h2>
-      <div className="grid grid-cols-3 gap-2 mb-5">
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 0);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 1);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 2);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 3);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 4);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 5);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 6);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 7);
-          }}
-        ></div>
-        <div
-          className={`bg-slate-500 min-w-20 h-20 rounded-md flex items-center justify-center text-white font-bold text-2xl`}
-          onClick={(e) => {
-            toggle(e, 8);
-          }}
-        ></div>
-      </div>
-      <button className="px-3 py-2 mx-auto bg-red-600 font-semibold uppercase rounded-md text-white hover:bg-red-700 active:scale-90 transition">
-        Reset
-      </button>
+      <TicTacToe />
     </div>
   );
 }
---
+++ src/conponents/TicTacToe.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+
+function TicTacToe() {
+  const [gameBoard, setGameBoard] = useState(Array(9).fill(""));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const handleClick = (i) => {
+    const newGameBoard = [...gameBoard];
+    newGameBoard[i] = currentPlayer;
+    setGameBoard(newGameBoard);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+
+    const newWinner = checkWinner(newGameBoard);
+    newWinner ? setWinner(newWinner) : isMatchCompleted(setWinner("Draw!"));
+  };
+
+  const checkWinner = (gameBoard) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const condition of winConditions) {
+      const [a, b, c] = condition;
+      if (
+        gameBoard[a] &&
+        gameBoard[a] === gameBoard[b] &&
+        gameBoard[a] === gameBoard[c]
+      ) {
+        return gameBoard[a];
+      }
+    }
+    return null;
+  };
+
+  const isMatchCompleted = (board) => {
+    return board.every((cell) => cell !== "");
+  };
+
+  return (
+    <>
+      <div className="my-5 text-xl">
+        <h4 className="text-white">
+          Player Turn : <span className="text-lime-500">{currentPlayer}</span>
+        </h4>
+      </div>
+      <div className="grid grid-cols-3 gap-2 mb-5">
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(0);
+          }}
+        >
+          {gameBoard[0]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(1);
+          }}
+        >
+          {gameBoard[1]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(2);
+          }}
+        >
+          {gameBoard[2]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(3);
+          }}
+        >
+          {gameBoard[3]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(4);
+          }}
+        >
+          {gameBoard[4]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(5);
+          }}
+        >
+          {gameBoard[5]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(6);
+          }}
+        >
+          {gameBoard[6]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(7);
+          }}
+        >
+          {gameBoard[7]}
+        </div>
+        <div
+          className="box"
+          onClick={() => {
+            handleClick(8);
+          }}
+        >
+          {gameBoard[8]}
+        </div>
+      </div>
+      <button className="btn">Reset</button>
+    </>
+  );
+}
+
+export default TicTacToe;
